fcf7608ae61e508a280ffd15073a5228d1c6b405
Kargrig <kargriga7@gmail.com>
2021-12-12 11:26:51 +0400

    Second

---
+++ src/week4/Task25.java
@@ -0,0 +1,18 @@
+package week4;
+
+import java.util.Scanner;
+
+public class Task25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int x = sc.nextInt();
+        int count = 0;
+            for (int i = 1; i<=x; i++){
+                if (x % i ==0){
+                    count+=1;
+                    System.out.println("Your numbers is: "+ i);
+                }
+            }
+    }
+}
